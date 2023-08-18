d3666a44fdff62bddcc8e79a0e3ead795ffda516
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-18 17:54:39 -0300

    Update clase1-tarea1.js

--- clase1-tarea1.js
+++ clase1-tarea1.js
@@ -2,8 +2,8 @@ function calcularEdad(anioActual, anioNacimiento) {
     return anioActual - anioNacimiento;
 }
 
-const anioActual = Number(prompt(2023));
-const anioNacimiento = Number(prompt(1998));
+const anioActual = 2023(prompt(2023));
+const anioNacimiento = 1998(prompt(1998));
 
 console.log('Tenés ' + calcularEdad(2023 - 1998);//Tarea 1
 
